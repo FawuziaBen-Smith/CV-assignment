a94db1466fca7ff788b63f871ff2ee184b15c432
Fawuzia Ben-Smith <fawuzia.smith@ashesi.edu.gh>
2020-09-23 00:47:51 +0000

    cv

---
+++ CV.js
@@ -0,0 +1,22 @@
+
+    
+
+
+        
+
+		function submit(){
+            localStorage["name"] = document.getElementById("name").value;
+			localStorage["lname"]= document.getElementById("Mname").value;
+			localStorage["Mname"]= document.getElementById("lname").value;
+			localStorage["gender"]= document.getElementById("gender").text;
+			localStorage["Contact"]= document.getElementById("Contact").value;
+			localStorage["email"]= document.getElementById("email").value;
+			localStorage["address"]= document.getElementById("address").value;
+		
+			window.location.replace("CV Page.html"); 
+	
+        
+        
+
+			l
+		
